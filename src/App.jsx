29cf8ad30c05ca522870d16a9c3c2c792a0b7fd3
Richard Hemba <richardhemba66@gmail.com>
2025-06-26 10:29:54 +0100

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AboutPage from "./pages/about"
 
 function App() {
 
   return (
     <>
-    
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AboutPage />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
